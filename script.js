06d4e84ceccca71da643c25e8e5d529941cc8a23
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-04-22 16:53:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 let lists = document.querySelector("#band");
-
+console.log(lists, "abc")
 bands.sort((a,b) => {
 	if(a > b) return 1;
 	else return -1;
 })
-let newElem = document.createElement('li');
-newParagraph.style.cssText = 'padding : 5; list-style : square`
-
 for(let b of bands){
-	newElem.innerText = b;
-	lists.append(newElem);
+	 let newElem = document.createElement('li');
+	newElem.style.cssText = 'padding : 8px; list-style : square;'
+    newElem.innerText = b;
+    lists.appendChild(newElem); 
+	lists.appendChild(document.createElement('hr'));
 }
 
-console.log(bands);
+console.log(lists);
