f21d05a60fa85c9af25b83f05e371697184314e6
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-04-22 16:43:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+let lists = document.querySelector("#band");
+
+bands.sort((a,b) => {
+	if(a > b) return 1;
+	else return -1;
+})
+let newElem = document.createElement('li');
+newParagraph.style.cssText = 'padding : 5; list-style : square`
+
+for(let b of bands){
+	newElem.innerText = b;
+	lists.append(newElem);
+}
+
+console.log(bands);
